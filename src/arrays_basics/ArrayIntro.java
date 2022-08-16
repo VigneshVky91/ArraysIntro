456560bc28309fc8cd18696cb4c03a4b962d8090
Vignesh.manoharan <vignesh.manoharan@stackroute.in>
2022-08-16 22:56:43 +0530

    16th Aug 2022

---
+++ src/arrays_basics/ArrayIntro.java
@@ -0,0 +1,22 @@
+package arrays_basics;
+
+public class ArrayIntro {
+		
+	public static void main(String[] args) {
+		int arr[] = new int[5];
+		arr[0] = 235;
+		arr[1] = 15;
+		arr[2] = 37;
+		arr[3] = 45;
+		arr[4] = 95;
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+//		System.out.println(arr[5]);
+
+
+	}
+}
